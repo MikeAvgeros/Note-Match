3ee0c38f0f3a262dddde168f9e57e85770b864be
Mike Avgeros <mike.avgeros@gmail.com>
2020-08-25 20:35:54 +0100

    Minor adjustments

--- Assets/Scripts/NoteQuiz.cs
+++ Assets/Scripts/NoteQuiz.cs
@@ -111,7 +111,7 @@ public class NoteQuiz : MonoBehaviour
     private IEnumerator ButtonFinder(UIButton button)
     {
         yield return new WaitForSeconds(1.5f);
-        button.GetComponent<Image>().color = Color.red;
+        button.GetComponent<Image>().color = Color.yellow;
         yield return new WaitForSeconds(1f);
         button.GetComponent<Image>().color = Color.white;
     }
