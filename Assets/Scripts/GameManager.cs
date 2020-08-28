4b45303e6b65661a68206fb7ae96eab94fe65a1c
Mike Avgeros <mike.avgeros@gmail.com>
2020-08-28 12:54:52 +0100

    Minor adjustments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,7 +24,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = -1;
         if (instance != null & instance != this)
         {
             Destroy(gameObject);
